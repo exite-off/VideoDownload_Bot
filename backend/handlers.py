9a7a3a7e62772963ba5c4156a4c38631811633aa
exite-off <vfedin868@gmail.com>
2025-08-22 00:01:39 +0300

    Initial commit

---
+++ backend/handlers.py
@@ -0,0 +1,10 @@
+﻿from aiogram.types import Message
+from aiogram import F, Router
+
+router = Router()
+
+# Handler for command /start
+@router.message(F.text)
+async def echo(message: Message) -> None:
+    await message.answer(message.text)
+
---
+++ main.py
@@ -0,0 +1,19 @@
+﻿import asyncio
+from aiogram import Bot, Dispatcher
+from backend.handlers import router
+import os
+
+bot = Bot(token=os.environ['BOT_TOKEN'])
+dp = Dispatcher()
+
+# Main function with bot polling
+async def main() -> None:
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+# Infinite loop for main function
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
