4138c0f5422f9b49506f34a455399abad8fea364
exite-off <vfedin868@gmail.com>
2025-08-22 12:20:13 +0300

    Added cookies simulation for bot check bypass

--- functions.py
+++ functions.py
@@ -1,9 +1,15 @@
 ﻿import ytmusicapi
 import yt_dlp
 
+# configure yt_dlp
+base_url = 'https://music.youtube.com/watch?v='
+yt = ytmusicapi.YTMusic()
+cookie_file_path = 'cookies.txt'
+
 # Download audio from YouTube music
 async def download_audio(yt_url, query = 'result') -> None:
     ydl_opts = {
+        'cookies': cookie_file_path,
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -14,7 +20,3 @@ async def download_audio(yt_url, query = 'result') -> None:
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([yt_url])
-
-# configure yt_dlp
-base_url = 'https://music.youtube.com/watch?v='
-yt = ytmusicapi.YTMusic()
